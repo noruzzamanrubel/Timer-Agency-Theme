14c0ef6d036fe6b37281f2885d74e6f27164a440
noruzzaman rubel <noruzzamanrubel@gmail.com>
2020-10-05 16:20:16 +0600

    update

--- contact.php
+++ contact.php
@@ -45,34 +45,7 @@ get_header();?>
                     </p>
                     <div class="contact-form">
                         <form id="contact-form" method="#" action="#" role="form">
-                
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".6s">
-                                <input type="text" placeholder="Your Name" class="form-control" name="name" id="name">
-                            </div>
-                            
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay=".8s">
-                                <input type="email" placeholder="Your Email" class="form-control" name="email" id="email" >
-                            </div>
-                            
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1s">
-                                <input type="text" placeholder="Subject" class="form-control" name="subject" id="subject">
-                            </div>
-                            
-                            <div class="form-group wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1.2s">
-                                <textarea rows="6" placeholder="Message" class="form-control" name="message" id="message"></textarea>    
-                            </div>
-                            
-                            <div id="success" class="success">
-                                Thank you. The Mailman is on His Way :)
-                            </div>
-                            
-                            <div id="error" class="error">
-                                Sorry, don't know what happened. Try later :(
-                            </div>
-                            
-                            <div id="submit" class="wow fadeInDown" data-wow-duration="500ms" data-wow-delay="1.4s">
-                                <input type="submit" id="contact-submit" class="btn btn-default btn-send" value="Send Message">
-                            </div>                      
+                            <?php the_content() ?>                     
                         </form>
                     </div>
                 </div>
